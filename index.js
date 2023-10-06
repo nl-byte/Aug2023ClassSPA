bfa2938b3231b4e342a7b13f56921ce175097793
nl-byte <lawasnichole.nl@gmail.com>
2023-10-05 21:39:16 -0400

    week7spa404errorupdate

--- index.js
+++ index.js
@@ -14,61 +14,84 @@ function render(state = store.Home) {
     ${Footer()}
   `;
   afterRender();
-   router.updatePageLinks();
+  router.updatePageLinks();
 }
 
 function afterRender() {
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
 }
 
 router.hooks({
   before: (done, params) => {
-    // We need to know what view we are on to know what data to fetch
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
-    // Add a switch case statement to handle multiple route
-     switch (view) {
-      // Add a case for each view that needs data from an API
-      case "Pizza":
-        // New Axios get request utilizing already made environment variable
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Home":
         axios
-          .get(process.env.PIZZA_PLACE_API_URL_)
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
           .then(response => {
-            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
-            console.log("response", response);
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            store.Home.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+        break;
+      // Added in Lesson 7.1
+      case "Pizza":
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then(response => {
             store.Pizza.pizzas = response.data;
             done();
           })
-          .catch((error) => {
+          .catch(error => {
             console.log("It puked", error);
             done();
           });
-          break;
-      default :
+        break;
+      default:
         done();
     }
   },
-  already: (params) => {
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
 
     render(store[view]);
   }
 });
-
 router
-.on({
-  "/": () => render(),
-  ":view": (params) => {
-    let view = capitalize(params.data.view);
-    if (view in store) {
-      render(store[view]);
-    } else {
-      render(store.Viewnotfound);
-      console.log(`View ${view} not defined`);
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
     }
-  },
-})
-.resolve();
-
+  })
+  .resolve();
